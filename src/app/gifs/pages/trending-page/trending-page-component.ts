7424541a6a79e2ff60fd1802e5894ca41edd7418
Zarkprogame <zarkprogame@gmail.com>
2025-09-09 12:37:43 +0200

    Diseño Masonry, Infinit Scroll, Guardar Scroll

--- src/app/gifs/pages/trending-page/trending-page-component.ts
+++ src/app/gifs/pages/trending-page/trending-page-component.ts
@@ -1,13 +1,41 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, viewChild } from '@angular/core';
 import { GifListComponent } from "../../components/gif-list/gif-list";
 import { GifService } from '../../services/gifs.service';
+import { ScrollStateService } from 'src/app/shared/services/scroll-state.service';
 
 @Component({
   selector: 'app-trending-page-component',
-  imports: [GifListComponent],
+  //imports: [GifListComponent],
   templateUrl: './trending-page-component.html',
 })
-export default class TrendingPageComponent {
+export default class TrendingPageComponent implements AfterViewInit {
 
   gifService = inject(GifService);
+  scrollStateService = inject(ScrollStateService);
+
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if( !scrollDiv ) return;
+
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+  }
+
+  onScroll( event: Event) {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if( !scrollDiv ) return;
+
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight;
+    const scrollHeight = scrollDiv.scrollHeight;
+
+    const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
+
+    this.scrollStateService.trendingScrollState.set(scrollTop);
+
+    if( isAtBottom ){
+      this.gifService.loadTrendingGifs();
+    }
+  }
 }
